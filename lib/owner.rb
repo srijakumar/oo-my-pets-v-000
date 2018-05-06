bcbe927181022c2400f02562a2565b09e9958a89
Srija S <srijakumar7@gmail.com>
2018-05-06 23:49:13 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -22,7 +22,17 @@ def initialize (species)
     @pets = {:fishes => [], :dogs => [], :cats => []}
 end
 
+def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+  end
 
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
 
 def say_species
   "I am a #{species}"
