17653695289e84c0af834d627933c1eadba36d24
Srija S <srijakumar7@gmail.com>
2018-05-06 23:51:43 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,6 +34,24 @@ def buy_fish(name)
     pets[:cats] << Cat.new(name)
   end
 
+  def walk_dogs
+      pets[:dogs].each do |dog|
+        dog.mood = "happy"
+      end
+    end
+
+    def play_with_cats
+      pets[:cats].each do |cat|
+        cat.mood = "happy"
+      end
+    end
+
+    def feed_fish
+      pets[:fishes].each do |fish|
+        fish.mood = "happy"
+      end
+    end
+
 def say_species
   "I am a #{species}"
 end
